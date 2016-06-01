4e18ff89796263d1aeaad35bb2727a8debff9e3e
Axel Verbeiren <axel.verbeiren@gmail.com>
2016-06-01 23:04:36 +0200

    hideKeyboard

--- web/bundles/smsite/js/site-part.js
+++ web/bundles/smsite/js/site-part.js
@@ -25,6 +25,18 @@ function get_part_html_put_content(id_from,path,id_to_put)
     }
 }
 
+(function ($) {
+    $.fn.hideKeyboard = function() {
+        var inputs = this.filter("input").attr('readonly', 'readonly'); // Force keyboard to hide on input field.
+        var textareas = this.filter("textarea").attr('disabled', 'true'); // Force keyboard to hide on textarea field.
+        setTimeout(function() {
+            inputs.blur().removeAttr('readonly');  //actually close the keyboard and remove attributes
+            textareas.blur().removeAttr('disabled');
+        }, 100);
+        return this;
+    };
+}( jQuery ));
+
 
 function post_form(path,data)
 {
